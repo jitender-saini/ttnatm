b48eaa86a0aad3ff8546f18185cf73ff47697c9b
imthapa <ishwarmani.thapa@tothenew.com>
2017-03-07 15:42:42 +0530

    ankits coomit

--- index.js
+++ index.js
@@ -42,3 +42,25 @@ function util() {
   console.log(two,five,one);
   console.log(first.twoK  );
 }
+
+
+function Transaction(two,five,one) {
+  this.twoK = two;
+  this.fiveHundred = five;
+  this.oneHundred = one;
+
+  this.computeTotalAmt = function(){
+    var totAmt = (this.twoK)*2000+(this.fiveHundred)*500+(this.oneHundred)*100;
+    return totAmt;
+  }
+
+}
+
+function transact() {
+  var two=parseInt(document.getElementById("two-thousand").value);
+  var five=parseInt(document.getElementById("five-hundred").value);
+  var one=parseInt(document.getElementById("one-hundred").value);
+  var obj = new Transaction();
+  //console.log(obj);
+
+}
